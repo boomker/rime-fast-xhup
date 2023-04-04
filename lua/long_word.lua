6a5b545a7f5c28a41228ea95e2ded0b4617d8377
zhuchunyan <gmboomker@gmail.com>
2023-04-04 22:38:11 +0800

    improve: 词频优化, 添加少量简拼支持, 去除偏僻读音, 自定义候选提示

--- lua/long_word.lua
+++ lua/long_word.lua
@@ -1,34 +1,55 @@
--- 长词优先（从后方移动2个英文候选和3个中文长词，提前为第2-6候选；当后方候选长度全部不超过第一候选词时，不产生作用）
-local function long_word_filter(input)
-	local l = {}
+---@diagnostic disable: undefined-global
+local function long_word_filter(input, env)
+	local cands = {}
 	-- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
 	local length = 0
-	-- 记录筛选了多少个英语词条(只提升3个词的权重，并且对comment长度过长的候选进行过滤)
-	local s1 = 0
-	-- 记录筛选了多少个汉语词条(只提升3个词的权重)
-	local s2 = 0
+	-- 记录筛选了多少个英语词条(只提升2个词的权重，并且对comment长度过长的候选进行过滤)
+	-- local s_onlyEN = 0
+	-- 记录筛选了多少个汉语词条(只提升2个词的权重)
+	local s_onlyCN = 0
+	local sCNEN = 0
+	local commit = env.engine.context:get_commit_text()
 	for cand in input:iter() do
-		leng = utf8.len(cand.text)
+		local leng = utf8.len(cand.text)
 		if length < 1 then
-			length = leng
+			length = leng or 0
 			yield(cand)
-		elseif #table > 30 then
-			table.insert(l, cand)
-		elseif ((leng > length) and (s1 < 1)) and (string.find(cand.text, "^[%w%p%s]+$")) then
-			s1 = s1 + 1
-			if string.len(cand.text) / string.len(cand.comment) > 1.5 then
+
+			if (string.find(cand.text, "[^x00-xff]+")) then
+                FirstWordType = "chinese"
+			end
+		--[[ elseif string.len(cand.text) > 15 then
+			table.insert(cands, cand)]]
+		elseif string.find(cand.text, "^[%w%u%l%s·-]+$") and FirstWordType ~= "chinese" then
+			if
+				string.len(cand.text) / string.len(commit) > 1.3
+				and string.len(cand.text) / string.len(commit) <= 2.5
+				and string.len(cand.comment) <= 4
+				and string.len(commit) >= 3
+			then
 				yield(cand)
 			end
-		elseif ((leng > length) and (s2 < 2)) and (string.find(cand.text, "^[%w%p%s]+$") == nil) then
-			yield(cand)
-			s2 = s2 + 1
+		elseif ((leng > length) and (s_onlyCN < 2)) and (string.find(cand.text, "[%w%u]") == nil) then
+			if string.len(commit) > 2 then
+				yield(cand)
+				s_onlyCN = s_onlyCN + 1
+			end
+		elseif
+			((leng > length) and (sCNEN < 2))
+			and (string.find(cand.text, "%w%u+"))
+			and (string.find(cand.text, "[^x00-xff]+"))
+		then
+			if string.len(commit) > 3 then
+				yield(cand)
+				sCNEN = sCNEN + 1
+			end
 		else
-			table.insert(l, cand)
+			table.insert(cands, cand)
 		end
 	end
-	for i, cand in ipairs(l) do
+	for _, cand in ipairs(cands) do
 		yield(cand)
 	end
 end
 
-return {longwordfilter=long_word_filter}
+return { longwordfilter = long_word_filter }
