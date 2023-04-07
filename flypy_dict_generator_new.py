84a6d4f15f3e01e44a6fea0ec345fb45e9e2171e
zhuchunyan <gmboomker@gmail.com>
2023-04-08 02:57:16 +0800

    添加二字词语音形码词库, 脚本支持形码转换

--- flypy_dict_generator_new.py
+++ flypy_dict_generator_new.py
@@ -3,6 +3,7 @@
 import sys
 from pathlib import PosixPath as pp
 from functools import lru_cache
+from xhxm_map import xhxm_dict
 # from pypinyin import lazy_pinyin
 # from datetime import date
 
@@ -52,25 +53,16 @@ def pinyin_to_flypy(pinyin: list[str]):
             "en": "en",
             "eng": "eg",
         }
-        if pinyin in zero.keys():
+        if pinyin in zero:
             return zero[pinyin]
+        if pinyin[1] == "h":
+            shengmu = shengmu_dict[pinyin[:2]]
+            yunmu = yunmu_dict[pinyin[2:]] if pinyin[2:] in yunmu_dict else pinyin[2:]
+            return shengmu + yunmu
         else:
-            if pinyin[1] == "h":
-                shengmu = shengmu_dict[pinyin[:2]]
-                yunmu = (
-                    yunmu_dict[pinyin[2:]]
-                    if pinyin[2:] in yunmu_dict.keys()
-                    else pinyin[2:]
-                )
-                return shengmu + yunmu
-            else:
-                shengmu = pinyin[:1]
-                yunmu = (
-                    yunmu_dict[pinyin[1:]]
-                    if pinyin[1:] in yunmu_dict.keys()
-                    else pinyin[1:]
-                )
-                return shengmu + yunmu
+            shengmu = pinyin[:1]
+            yunmu = yunmu_dict[pinyin[1:]] if pinyin[1:] in yunmu_dict else pinyin[1:]
+            return shengmu + yunmu
 
     return [to_flypy(x) for x in pinyin]
 
@@ -84,15 +76,21 @@ def quanpin_to_flypy(line_content, *args):
         pinyin_list = [i for i in _pys if i.isascii() and i.isalpha()]
     else:
         pinyin_list = [i for i in contents_perline if i.isascii() and i.isalpha()]
+    
     if pinyin_list:
         print("pinyin_list: ", pinyin_list)
+        # print("xhxm_list: ", words_xm_list)
         flypy_list = pinyin_to_flypy(pinyin_list)
         word_frequency = (
-            f"\t{contents_perline[-1]}" if contents_perline[-1].isnumeric() else ""
+            f"\t{contents_perline[-1]}" if contents_perline[-1].isnumeric() else "\t1"
         )
-        xhup_str = " ".join(flypy_list)
-        item = f"{contents_perline[0].strip()}\t{xhup_str}{word_frequency}\n"
-        yield item
+        if args[1]:
+            words_xm_list = [ xhxm_dict.get(m, "[") for m in contents_perline[0].strip() ]
+            xhup_list = [ "[".join([e, x]) for e, x in zip(flypy_list, words_xm_list) ]
+            xhup_str = " ".join(xhup_list)
+        else:
+            xhup_str = " ".join(flypy_list)
+        yield f"{contents_perline[0].strip()}\t{xhup_str}{word_frequency}\n"
 
 
 def write_date_to_file(data, outfile):
@@ -113,16 +111,15 @@ def write_date_to_file(data, outfile):
         ...
     """
 
-    if outfile not in globals().keys():
+    if outfile in globals():
+        with open(outfile, "a") as odf:
+            odf.write(data)
+    else:
         with open(outfile, "w") as odf:
             # odf.write(dict_header)
             odf.write("\n".join([c.lstrip() for c in dict_header.splitlines()]))
             odf.write("\n")
-    else:
-        with open(outfile, "a") as odf:
-            odf.write(data)
-
-    globals().update({outfile: True})
+    globals()[outfile] = True
 
 
 def open_dict_and_send_line(infile):
@@ -143,13 +140,15 @@ def open_dict_and_send_line(infile):
 
 
 def main():
-    hanzi2pinyin = sys.argv[-1]
-    option = None if hanzi2pinyin.endswith("yaml") else hanzi2pinyin
+    cli_args = sys.argv
+    option1 = None if cli_args[-1].endswith("yaml") else cli_args[-1]
+    option2 = "xm" if cli_args[-2].endswith("xm") else None
+    option2 = "xm" if option1 == "xm" else option2
 
     pp_objs = [pp(sys.argv[i]) for i in range(1, len(sys.argv))]
     infile_names = [f for f in pp_objs if f.is_file()]
     outfile_names = [f"flypy_{f.name.split('.')[0]}.dict.yaml" for f in infile_names]
-    print(infile_names, outfile_names, option)
+    print(infile_names, outfile_names, option1, option2)
     print(
         """当你只看到上的回显提示,脚本就结束了, 那么说明命令行参数出问题了.
           当词典文件没有附带拼音, 那么`option`需要设非空值"""
@@ -158,7 +157,7 @@ def main():
 
     for outfile, indata in zip(outfile_names, input_datas):
         for idata in indata:
-            for odata in quanpin_to_flypy(idata, option):
+            for odata in quanpin_to_flypy(idata, option1, option2):
                 write_date_to_file(odata, outfile)
 
 
