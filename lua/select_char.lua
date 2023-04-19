a52cbe5e04b321653e41f102282e55a17fc515bc
zhuchunyan <gmboomker@gmail.com>
2023-04-19 21:53:51 +0800

    improve: 单字全码顶屏 二字词形码唯一顶屏 &  & update dict

--- lua/select_char.lua
+++ lua/select_char.lua
@@ -2,6 +2,12 @@
 -- https://github.com/BlindingDark/rime-lua-select-character
 -- 删除了默认按键，需要在 key_binder（default.custom.yaml）下设置
 -- http://lua-users.org/lists/lua-l/2014-04/msg00590.html
+
+
+-- local puts = require("tools/debugtool")
+
+require("tools/metatable")
+
 local function utf8_sub(s, i, j)
 	i = i or 1
 	j = j or -1
@@ -54,15 +60,94 @@ local function last_character(s)
 end
 
 local function select_character(key, env)
+    -- local reverse_dict = ReverseDb("build/flypy_xhfast.reverse.bin") -- 从编译文件中获取反查词库
+    -- local mem = Memory(env.engine,env.engine.schema)
 	local engine = env.engine
+	local config = engine.schema.config
 	local context = engine.context
 	local commit_text = context:get_commit_text()
-	local config = engine.schema.config
+	local input_code = context.input
+    -- local composition = cntext.composition
 
-	-- local first_key = config:get_string('key_binder/select_first_character') or 'bracketleft'
-	-- local last_key = config:get_string('key_binder/select_last_character') or 'bracketright'
+	local first_cand_key = config:get_string('key_binder/select_first_cand') or 'space'
+	local second_cand_key = config:get_string('key_binder/select_second_cand') or 'semicolon'
+	local third_cand_key  = config:get_string('key_binder/select_third_character') or 'apostrophe'
 	local first_key = config:get_string("key_binder/select_first_character")
 	local last_key = config:get_string("key_binder/select_last_character")
+
+    --[[ if key:release() or key:ctrl() or key:alt() then
+        return 2
+    end
+
+    local keycode = key.keycode
+
+    if keycode  < 0x20 or keycode  >= 0x7f then
+        return 2
+    end
+
+    local key_char = string.char(keycode)
+    if keycode and string.find(input_code, "^[%l]+%[[%l]+$") then
+        -- local key_char = string.char(keycode)
+        local commit_code = reverse_dict:lookup(commit_text) or "" -- 待自动上屏的候选项编码
+        -- local xm = string.sub(commit_code, -2, -1)
+        local xm_last = string.sub(commit_code, -1)
+        if  string.find(xm_last, key_char) then
+            engine:commit_text(commit_text)
+            context:clear()
+
+            return 1 -- kAccepted
+        end
+
+        --[[ local cands = {}
+        puts(INFO, "||||||||", input_code, #cands)
+        mem:dict_lookup(input_code, true, 120)
+        for entry in mem:iter_dict() do
+            table.insert(cands, entry.text)
+            puts(INFO, "--------",entry.text, #cands)
+        end
+        if #cands == 1 then
+            engine:commit_text(commit_text)
+            context:clear()
+        end ]]
+    -- end ]]
+
+    if key:repr() == first_cand_key and string.find(input_code, "^[%w]+%[$") then
+		engine:commit_text(string.sub(commit_text, 1, -2))
+		context:clear()
+
+		return 1 -- kAccepted
+    elseif key:repr() == second_cand_key and string.find(input_code, "^[%w]+%[$") then
+        context:select(1)
+
+	    local second_cand_text = context:get_commit_text()
+        -- puts(INFO, "-----", second_cand_text )
+		engine:commit_text(utf8_sub(second_cand_text, 1, -2))
+		context:clear()
+		return 1 -- kAccepted
+    elseif key:repr() == third_cand_key and string.find(input_code, "^[%w]+%[$") then
+        context:select(2)
+
+        local third_cand_text = context:get_commit_text()
+        engine:commit_text(utf8_sub(third_cand_text , 1, -2))
+        context:clear()
+        return 1 -- kAccepted
+    end
+
+    if key:repr() == second_cand_key and string.find(input_code, "^%u[%u%l]+$") then
+        context:select(1)
+
+	    local second_cand_text = context:get_commit_text()
+		engine:commit_text(second_cand_text)
+		context:clear()
+		return 1 -- kAccepted
+    elseif key:repr() == third_cand_key and string.find(input_code, "^%u[%u%l]+$") then
+        context:select(2)
+
+	    local second_cand_text = context:get_commit_text()
+		engine:commit_text(second_cand_text)
+		context:clear()
+		return 1 -- kAccepted
+    end
 
 	if key:repr() == first_key and commit_text ~= "" then
 		engine:commit_text(first_character(commit_text))
