13d7889d9cfca5f9d6d9ab917a0dfbb56935805d
zhuchunyan <gmboomker@gmail.com>
2023-04-19 21:53:51 +0800

    improve: 单字全码顶屏 二字词形码唯一顶屏 &  & update dict

--- lua/select_char.lua
+++ lua/select_char.lua
@@ -7,7 +7,6 @@
 -- local puts = require("tools/debugtool")
 
 require("tools/metatable")
--- local code_text_tables = {}
 
 local function utf8_sub(s, i, j)
 	i = i or 1
@@ -61,12 +60,13 @@ local function last_character(s)
 end
 
 local function select_character(key, env)
-    local reverse_dict = ReverseDb("build/flypy_xhfast.reverse.bin") -- 从编译文件中获取反查词库
+    -- local reverse_dict = ReverseDb("build/flypy_xhfast.reverse.bin") -- 从编译文件中获取反查词库
+    -- local mem = Memory(env.engine,env.engine.schema)
 	local engine = env.engine
 	local config = engine.schema.config
 	local context = engine.context
 	local commit_text = context:get_commit_text()
-	local code_text = context.input
+	local input_code = context.input
     -- local composition = cntext.composition
 
 	local first_cand_key = config:get_string('key_binder/select_first_cand') or 'space'
@@ -75,7 +75,7 @@ local function select_character(key, env)
 	local first_key = config:get_string("key_binder/select_first_character")
 	local last_key = config:get_string("key_binder/select_last_character")
 
-    if key:release() or key:ctrl() or key:alt() then
+    --[[ if key:release() or key:ctrl() or key:alt() then
         return 2
     end
 
@@ -86,58 +86,37 @@ local function select_character(key, env)
     end
 
     local key_char = string.char(keycode)
-    --[[ puts(INFO, #code_text_tables, code_text, key_char)
-    if #code_text_tables == 4 or #code_text_tables == 5  then
-        code_text = code_text .. key_char
-        puts(INFO, "--------", code_text)
-    end
-    table.insert(code_text_tables, code_text)
-    puts(INFO, "++++++++", code_text) ]]
-    if keycode and string.find(code_text, "^[%l]+%[[%l]+$") then
+    if keycode and string.find(input_code, "^[%l]+%[[%l]+$") then
         -- local key_char = string.char(keycode)
         local commit_code = reverse_dict:lookup(commit_text) or "" -- 待自动上屏的候选项编码
         -- local xm = string.sub(commit_code, -2, -1)
-        -- puts(INFO, "||||||||", code_text, xm, key_char, string.find(xm, key_char))
         local xm_last = string.sub(commit_code, -1)
         if  string.find(xm_last, key_char) then
             engine:commit_text(commit_text)
             context:clear()
 
-            -- code_text_tables = {}
             return 1 -- kAccepted
         end
 
-        --[[
-        local cands = {}
-        for entry in mem:iter_user() do
+        --[[ local cands = {}
+        puts(INFO, "||||||||", input_code, #cands)
+        mem:dict_lookup(input_code, true, 120)
+        for entry in mem:iter_dict() do
             table.insert(cands, entry.text)
+            puts(INFO, "--------",entry.text, #cands)
         end
         if #cands == 1 then
             engine:commit_text(commit_text)
             context:clear()
         end ]]
-    end
+    -- end ]]
 
-    if string.len(code_text) >= 5 and string.find(code_text, "^[%l]+%[[%l]+$") then
-        local commit_code = reverse_dict:lookup(commit_text) or "" -- 待自动上屏的候选项编码
-        local xm = string.sub(commit_code, -2, -1)
-        -- puts(INFO, "||||||||", code_text, commit_code)
-        if  xm  == string.sub(code_text, -2, -1) then
-            engine:commit_text(commit_text)
-            context:clear()
-
-            -- code_text_tables = {}
-            return 1 -- kAccepted
-        end
-    end
-
-
-    if key:repr() == first_cand_key and string.find(code_text, "^[%w]+%[$") then
+    if key:repr() == first_cand_key and string.find(input_code, "^[%w]+%[$") then
 		engine:commit_text(string.sub(commit_text, 1, -2))
 		context:clear()
 
 		return 1 -- kAccepted
-    elseif key:repr() == second_cand_key and string.find(code_text, "^[%w]+%[$") then
+    elseif key:repr() == second_cand_key and string.find(input_code, "^[%w]+%[$") then
         context:select(1)
 
 	    local second_cand_text = context:get_commit_text()
@@ -145,7 +124,7 @@ local function select_character(key, env)
 		engine:commit_text(utf8_sub(second_cand_text, 1, -2))
 		context:clear()
 		return 1 -- kAccepted
-    elseif key:repr() == third_cand_key and string.find(code_text, "^[%w]+%[$") then
+    elseif key:repr() == third_cand_key and string.find(input_code, "^[%w]+%[$") then
         context:select(2)
 
         local third_cand_text = context:get_commit_text()
@@ -154,14 +133,14 @@ local function select_character(key, env)
         return 1 -- kAccepted
     end
 
-    if key:repr() == second_cand_key and string.find(code_text, "^%u[%u%l]+$") then
+    if key:repr() == second_cand_key and string.find(input_code, "^%u[%u%l]+$") then
         context:select(1)
 
 	    local second_cand_text = context:get_commit_text()
 		engine:commit_text(second_cand_text)
 		context:clear()
 		return 1 -- kAccepted
-    elseif key:repr() == third_cand_key and string.find(code_text, "^%u[%u%l]+$") then
+    elseif key:repr() == third_cand_key and string.find(input_code, "^%u[%u%l]+$") then
         context:select(2)
 
 	    local second_cand_text = context:get_commit_text()
@@ -187,17 +166,4 @@ local function select_character(key, env)
 	return 2 -- kNoop
 end
 
--- 初始化
--- local function init(env)
-    -- local config = env.engine.schema.config
-
-    -- env.reverse_dict = ReverseDb("build/keydo.reverse.bin") -- 从编译文件中获取反查词库
-
-    -- env.has_auto_select = config:get_bool("speller/auto_select") or false -- 从设置中读取自动上屏设置
-    -- env.history_leader = config:get_string("repeat_history/input") -- 从设置中读取历史模式引导键
-    -- env.phonetics_code = config:get_string("topup/phonetics_code") -- 从设置中读取音码集合
-    -- env.stroke_code = config:get_string("topup/stroke_code") -- 从设置中读取形码集合
--- end
-
--- return { init = init, func =  select_character }
 return select_character
