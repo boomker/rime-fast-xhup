e3b17039d1378108310d3be80e0ae310541ed7d5
zhuchunyan <gmboomker@gmail.com>
2023-05-08 23:46:14 +0800

    improve: 实现三字词和四字词顶字上屏

--- lua/long_word_up.lua
+++ lua/long_word_up.lua
@@ -8,20 +8,26 @@ local function long_word_up(input, env)
 	local cands = {}
 	-- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
 	local first_word_length = 0
-	local first_word_quality = 0
     local count = 1
 	-- 记录筛选了多少个汉语词条(只提升1个词的权重)
 	local ocn_count = 0
+    local idx = 0
+    local second_cand_quality = 0
 	local preedit_code = env.engine.context:get_commit_text()
 	for cand in input:iter() do
 		local cand_per_length = utf8.len(cand.text)
 		local cand_per_quality = cand.quality
 
-		if first_word_length < 1 then
+		if (first_word_length < 1) or (idx <= 1) then
 			first_word_length = cand_per_length or 0
-	        first_word_quality = cand_per_quality or 0
+            idx = idx + 1
 
-			if (string.find(cand.text, "[^x00-xff]+")) then
+            if idx == 2 then
+                second_cand_quality = cand.quality
+			    table.insert(cands, cand)
+                -- puts(INFO, cand.text, cand.quality)
+
+            elseif (string.find(cand.text, "[^x00-xff]+")) then
                 FirstWordType = "chinese"
                 yield(cand)
             else
@@ -39,8 +45,8 @@ local function long_word_up(input, env)
 				yield(cand)
 			end
 		elseif (cand_per_length > first_word_length) and (cand_per_length > 3)
-            and (ocn_count < count) and (cand_per_quality * 10 > first_word_quality)
-            and (string.find(cand.text, "[%w%u]") == nil)
+            and (ocn_count < count) and (cand_per_quality  > second_cand_quality)
+            and string.len(cand.comment) < string.len(preedit_code)
             and string.len(preedit_code) > 2 then
 				yield(cand)
 				ocn_count = ocn_count + 1
