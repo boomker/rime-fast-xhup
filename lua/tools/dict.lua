c717c9f27470bc44803ebcb4c425de37b97ad39e
boomker <gmboomker@gmail.com>
2023-05-28 01:39:26 +0800

    update: lua scripts and dict

--- lua/tools/dict.lua
+++ lua/tools/dict.lua
@@ -26,50 +26,52 @@ end
 
 --]]
 
-List=require 'tools/list'
+List = require 'tools/list'
 -- tools/string.lua
-function utf8.sub(str,si,ei)
-  local function index(ustr,i)
-    return i>=0 and ( ustr:utf8_offset(i) or ustr:len() +1 )
-    or ( ustr:utf8_offset(i) or 1 )
+
+function utf8.sub(str, si, ei)
+  local function index(ustr, i)
+    return i >= 0 and (ustr:utf8_offset(i) or ustr:len() + 1)
+        or (ustr:utf8_offset(i) or 1)
   end
 
-  local u_si= index(str,si)
+  local u_si = index(str, si)
   ei = ei or str:utf8_len()
-  ei = ei >=0 and ei +1 or ei
-  local u_ei= index(str, ei ) -1
-  return str:sub(u_si,u_ei)
+  ei = ei >= 0 and ei + 1 or ei
+  local u_ei = index(str, ei) - 1
+  return str:sub(u_si, u_ei)
 end
-string.utf8_len= utf8.len
-string.utf8_offset=utf8.offset
-string.utf8_sub=utf8.sub
+
+string.utf8_len = utf8.len
+string.utf8_offset = utf8.offset
+string.utf8_sub = utf8.sub
 
 --  line= "word\twegiht"
 local function conv_line(line)
-    local word,weight = line:match("^(.*)\t%s*([%d]*).*")
-    local key = word:utf8_sub(1,1)
-    return key,word,weight
+  local word, weight = line:match("^(.*)\t%s*([%d]*).*")
+  local key = word:utf8_sub(1, 1)
+  return key, word, weight
 end
+
 local function word_weight(word)
-  return tonumber(word:match("^.*-([%d]*).*") )
+  return tonumber(word:match("^.*-([%d]*).*"))
 end
 
 warn("@on")
 
 local function load_essay(filename)
-  local fn=io.open(filename)
+  local fn = io.open(filename)
   if not fn then
     (log and log.warning or warn)(filename .. " not exist in path of user_data or shared_data")
     return
   end
 
-  local words={}
+  local words = {}
   for line in fn:lines() do
-
-    local data = line:gsub("\t","-")
-    local key= utf8.sub(data,1,1)
+    local data = line:gsub("\t", "-")
+    local key = utf8.sub(data, 1, 1)
     words[key] = type(words[key]) == "table" and words[key] or List()
-    words[key]:push( data )
+    words[key]:push(data)
   end
 
   fn:close()
@@ -84,55 +86,55 @@ local function load_essay(filename)
   return words
 end
 
-local M={}
+local M = {}
 function M:New(filename)
   --filename = filename or "essay.txt"
-  local words =  load_essay(filename)
+  local words = load_essay(filename)
   if words then
-    return setmetatable(words,self)
+    return setmetatable(words, self)
   end
 end
-M.__index=M
-setmetatable(M,{__call=M.New})
+
+M.__index = M
+setmetatable(M, { __call = M.New })
 
 
 
-local s_func=function(a,b) return word_weight(a)> word_weight(b) end
+local s_func = function(a, b) return word_weight(a) > word_weight(b) end
 
-function M:find_word( word)
-    local dict=self[word:utf8_sub(1,1)] or List()
-    --- sort check
-    if not dict._sorted then
-      dict:sort_self(s_func)
-      dict._sorted = true
-    end
-    --  sert end
+function M:find_word(word)
+  local dict = self[word:utf8_sub(1, 1)] or List()
+  --- sort check
+  if not dict._sorted then
+    dict:sort_self(s_func)
+    dict._sorted = true
+  end
+  --  sert end
 
-    local res_tab= dict
-    :select(function(elm) return elm:match("^" .. word .. ".+-.*" ) end )
- --   :sort_self(function(a,b) return word_weight(a) > word_weight(b) end )
-    :map(function(elm) return elm:match("^" .. word .. "(.*)-.*$") end )
-    return res_tab
+  local res_tab = dict
+      :select(function(elm) return elm:match("^" .. word .. ".+-.*") end)
+      --   :sort_self(function(a,b) return word_weight(a) > word_weight(b) end )
+      :map(function(elm) return elm:match("^" .. word .. "(.*)-.*$") end)
+  return res_tab
 end
 
 function M:reduce_find_word(word)
-    local res_tab= List()
-    while 0 < word:utf8_len()  do
-      self:find_word(word)
-      :reduce(function(elm,dict) return dict:push(elm) end ,res_tab)
+  local res_tab = List()
+  while 0 < word:utf8_len() do
+    self:find_word(word)
+        :reduce(function(elm, dict) return dict:push(elm) end, res_tab)
 
-      word= word:utf8_sub(2)
-    end
-    return res_tab
+    word = word:utf8_sub(2)
+  end
+  return res_tab
 end
 
-
 --function M:empty(word)
-  --local key=word:utf8_sub(1,1)
-  --return self[key] == nil or #self[key] < 2 or false
+--local key=word:utf8_sub(1,1)
+--return self[key] == nil or #self[key] < 2 or false
 --end
 function M:empty(word)
-  for w,wt in self:reduce_iter(word) do
+  for w, wt in self:reduce_iter(word) do
     return false
   end
   return true
@@ -140,33 +142,32 @@ end
 
 function M:word_iter(word)
   return coroutine.wrap(function()
-    local dict=self[word:utf8_sub(1,1)] or List()
+    local dict = self[word:utf8_sub(1, 1)] or List()
     --- sort check
     if not dict._sorted then
       dict:sort_self(s_func)
       dict._sorted = true
     end
     -- sort check
-    for i,elm in ipairs(dict) do
-      local w,wt = elm:match("^" .. word .. "(.+)-(%d*).*$" )
+    for i, elm in ipairs(dict) do
+      local w, wt = elm:match("^" .. word .. "(.+)-(%d*).*$")
       if w then
-        coroutine.yield( w,tonumber(wt))
+        coroutine.yield(w, tonumber(wt))
       end
     end
-  end )
+  end)
 end
 
 function M:reduce_iter(word)
   return coroutine.wrap(
-  function()
-    repeat
-      for w,wt in self:word_iter(word) do
-        coroutine.yield(w,wt)
-      end
-      word= word:utf8_sub(2)
-    until word == ""
-  end)
+    function()
+      repeat
+        for w, wt in self:word_iter(word) do
+          coroutine.yield(w, wt)
+        end
+        word = word:utf8_sub(2)
+      until word == ""
+    end)
 end
 
 return M
-
--- lua/tools/list.lua
+++ lua/tools/list.lua
@@ -40,25 +40,26 @@
 --
 --
 
-local M={}
-M.__index=M
+local M = {}
+M.__index = M
 function M:New(...)
-  local obj={...}
-  if #obj==1 and type(obj[1]) =="table" then
-	obj = {}
-	for i,v in ipairs(...) do
-      table.insert(obj,v)
-	end
+  local obj = { ... }
+  if #obj == 1 and type(obj[1]) == "table" then
+    obj = {}
+    for i, v in ipairs(...) do
+      table.insert(obj, v)
+    end
   end
-  obj=setmetatable(obj,self)
+  obj = setmetatable(obj, self)
   return obj:_init(...)
 end
+
 function M:_init(...)
-	return self
+  return self
 end
 
 --setmetatable(M,{__index=table, __call=init})
-setmetatable(M,{ __call=M.New})
+setmetatable(M, { __call = M.New })
 
 -- local metatab={__index=M}
 
@@ -68,36 +69,35 @@ setmetatable(M,{ __call=M.New})
 --  List(1,2,3):reduce( function(elm,org) tabel.inert(org,elm) ;return org end , {} ) -> return {1,2,3}
 --  List(2,3):reduce( function(elm,org) tabel.inert(org,elm) ;return org end , List(1)  ) -> return {1,2,3}(List)
 function M:reduce(func, org, ...)
-  for i,v in ipairs(self) do
-    org=func(v,org, ...)
+  for i, v in ipairs(self) do
+    org = func(v, org, ...)
   end
   return org
 end
+
 function M:each(func, ...)
-  for i,v in ipairs(self) do
-    local res=func(v,...)
+  for i, v in ipairs(self) do
+    local res = func(v, ...)
     if res then return res end
   end
 end
 
-
 function M:each_with_index(func, ...) -- func   function( elm, index , ... )
-  for i,v in ipairs(self) do
+  for i, v in ipairs(self) do
     func(v, i, ...)
   end
 end
 
-
 function M:class()
-	return getmetatable(self).__index
+  return getmetatable(self).__index
 end
 
-local map_null_func=function(elm) return elm end
+local map_null_func = function(elm) return elm end
 function M:map(func, ...)
-  func= func or map_null_func
-  local tab = self:class()()  -- {}
-  for i,v in ipairs(self) do
-    tab:push( func(v,...) )
+  func = func or map_null_func
+  local tab = self:class()() -- {}
+  for i, v in ipairs(self) do
+    tab:push(func(v, ...))
     --table.insert(tab, func(v, ...) )
   end
   return tab
@@ -105,11 +105,11 @@ function M:map(func, ...)
 end
 
 function M:map_with_index(func, ...)
-  func= func or map_null_func
+  func = func or map_null_func
   --local tab={}
-  local tab = self:class()()  -- {}
-  for i,v in ipairs(self) do
-    tab:push( func(v, v, ...) )
+  local tab = self:class()() -- {}
+  for i, v in ipairs(self) do
+    tab:push(func(v, v, ...))
     --table.insert(tab, func(v, i, ...) )
   end
   --setmetatable(tab,M)
@@ -119,39 +119,39 @@ function M:map_with_index(func, ...)
 end
 
 function M:select(func, ...)
-  local tab={}
-  for i,v in ipairs(self) do
-    if  func(v, ...) then table.insert(tab,v) end
-  end
-  return self:class()(tab)
-end
-function M:select_with_index(func, ...)
-  local tab={}
-  for i,v in ipairs(self) do
-    if  func(v,i, ...) then table.insert(tab,v) end
+  local tab = {}
+  for i, v in ipairs(self) do
+    if func(v, ...) then table.insert(tab, v) end
   end
   return self:class()(tab)
 end
 
-local function def_find(elm,argv)
-    return elm == argv
-end
-function M:find(func,...)
-  local argv=...
-  if type(func)~= "function" then
-    func,argv = def_find, func
+function M:select_with_index(func, ...)
+  local tab = {}
+  for i, v in ipairs(self) do
+    if func(v, i, ...) then table.insert(tab, v) end
   end
-  for i,v in ipairs(self) do
-    if func(v,argv) then
+  return self:class()(tab)
+end
+
+local function def_find(elm, argv)
+  return elm == argv
+end
+function M:find(func, ...)
+  local argv = ...
+  if type(func) ~= "function" then
+    func, argv = def_find, func
+  end
+  for i, v in ipairs(self) do
+    if func(v, argv) then
       return v
     end
   end
   return nil
 end
 
-
 -- not use
-local function deepcompare(t1,t2,ignore_mt)
+local function deepcompare(t1, t2, ignore_mt)
   local ty1 = type(t1)
   local ty2 = type(t2)
   if ty1 ~= ty2 then return false end
@@ -160,24 +160,25 @@ local function deepcompare(t1,t2,ignore_mt)
   -- as well as tables which have the metamethod __eq
   local mt = getmetatable(t1)
   if not ignore_mt and mt and mt.__eq then return t1 == t2 end
-  for k1,v1 in pairs(t1) do
+  for k1, v1 in pairs(t1) do
     local v2 = t2[k1]
-    if v2 == nil or not deepcompare(v1,v2) then return false end
+    if v2 == nil or not deepcompare(v1, v2) then return false end
   end
-  for k2,v2 in pairs(t2) do
+  for k2, v2 in pairs(t2) do
     local v1 = t1[k2]
-    if v1 == nil or not deepcompare(v1,v2) then return false end
+    if v1 == nil or not deepcompare(v1, v2) then return false end
   end
   return true
 end
 
 function M:reverse()
-  local obj=M()
-  for i= #self, 1 do 
-    obj[#self-i+1] = self[i] 
-  end 
+  local obj = M()
+  for i = #self, 1, -1 do
+    obj[#self - i + 1] = self[i]
+  end
   return obj
-end 
+end
+
 function M:size()
   return #self
 end
@@ -186,88 +187,87 @@ function M:empty()
   return #self == 0
 end
 
-
 function M:clear()
-  for i=1,#self do
-	  self[i]=nil
+  for i = 1, #self do
+    self[i] = nil
   end
   return self
 end
+
 function M:insert_check(elm)
   return nil ~= elm
 end
+
 function M:insert_at(index, elm)
-  assert(index , --and elm,
-    ("args error: %s, index: %d , elm: %s"):format( "List:insert_at(index,elm) ", index,elm) )
-  index = index >=0 and index or index  +1+#self
+  assert(index,  --and elm,
+    ("args error: %s, index: %d , elm: %s"):format("List:insert_at(index,elm) ", index, elm))
+  index = index >= 0 and index or index + 1 + #self
   if self:insert_check(elm) then
-    table.insert(self,index,elm)
-    return self,true
+    table.insert(self, index, elm)
+    return self, true
   end
 
-  return self,false
+  return self, false
 end
+
 function M:push(elm)
-  return self:insert_at(#self+1, elm)
+  return self:insert_at(#self + 1, elm)
 end
-M.append=M.push
-M.unpack=table.unpack
-M.concat=table.concat
+
+M.append = M.push
+M.unpack = table.unpack
+M.concat = table.concat
 function M:unshift(elm)
-  return self:insert_at(1,elm)
+  return self:insert_at(1, elm)
 end
 
 function M:remove_at(index)
-  index =  index > 0 and index or index + #self +1
-  if index< 1 then return nil,self end
-  return table.remove(self,index),self
+  index = index > 0 and index or index + #self + 1
+  if index < 1 then return nil, self end
+  return table.remove(self, index), self
 end
 
 function M:shift()
-  return table.remove(self,1),self
+  return table.remove(self, 1), self
 end
+
 function M:pop()
-  return table.remove(self),self
+  return table.remove(self), self
 end
-M.concat= table.concat
+
+M.concat = table.concat
 function M:clone()
-	return self:class()(
-	   self:map()
-	)
+  return self:class()(
+    self:map()
+  )
 end
-function M:sort_self(func,...)
-  table.sort(self,func)
+
+function M:sort_self(func, ...)
+  table.sort(self, func)
   return self
-end 
-function M:sort(func,...)
-  return self:clone():sort_self()
-end 
-  
-
-
-
-local append_func=function(elm,org) org:push(elm) ; return org end
-function M:__add(...)
-	return M(...):reduce(
-		append_func,
-		self:clone()
-		)
 end
+
+function M:sort(func, ...)
+  return self:clone():sort_self()
+end
+
+local append_func = function(elm, org)
+  org:push(elm); return org
+end
+function M:__add(...)
+  return M(...):reduce(
+    append_func,
+    self:clone()
+  )
+end
+
 function M:__shl(...)
-	return M(...):reduce(append_func,self)
+  return M(...):reduce(append_func, self)
 end
 
 function M:__eq(list)
   return #self == #list and
-         table.concat(self,"|") == table.concat(list,"|")
+      table.concat(self, "|") == table.concat(list, "|")
 end
 
-
-
-
-
-
-
 return M
-
-
