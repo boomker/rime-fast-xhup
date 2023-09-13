7ffbec538cf76157d80a71c235fc113f04821375
Shingo <boomker@users.noreply.github.com>
2023-09-13 15:06:58 +0800

    fix: 双引号

--- lua/cold_word_drop.lua
+++ lua/cold_word_drop.lua
@@ -161,13 +161,13 @@ function cold_word_drop.filter(input, env)
     local cands = {}
     local i = 1
     local idx = config:get_int("turn_down_freq_config/idx") or 3
-    if string.match(input_code, '^[,.;\'"()?:!@#^&+-%=%_]') and (#input_code == 1) then
-        engine:commit_text(input_code)
-        context:clear()
-        return 1
-    end
 
     for cand in input:iter() do
+        if string.match(input_code, '^[.,;()?:!@#^&+-%=%_]') and (#input_code == 1) then
+            engine:commit_text(cand.text)
+            context:clear()
+            return 1
+        end
         local cpreedit_code = string.gsub(cand.preedit, ' ', '')
         if (i <= idx) then
             local tfl = turndown_freq_list[cand.text] or nil
