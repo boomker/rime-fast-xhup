9acd02a32d389a2c816c5261db4dd439f49dc06b
Shingo <boomker@users.noreply.github.com>
2023-10-14 18:51:02 +0800

    feat: launcher 支持二级菜单

--- lua/launcher.lua
+++ lua/launcher.lua
@@ -1,6 +1,7 @@
 -- local puts = require("tools/debugtool")
 local launcher = {}
 local app_items = require("launcher_config")
+local of_items = nil
 
 local function cmd(order)
 	local osascript = order
@@ -23,18 +24,60 @@ function launcher.processor(key, env)
 	local segment = composition:back()
 	local input_code = context.input
 	local preedit_code_length = #input_code
+	local keyvalue = key:repr()
+
+    local selected_candidate_keyval_tbl = {
+        ["Escape"] = true,
+        ["Enter"] = "x",
+        ["Return"] = "x",
+        ["space"] = "x",
+        ["semicolon"] = 1,
+        ["apostrophe"] = 2,
+        ["1"] = 0,
+        ["2"] = 1,
+        ["3"] = 2,
+        ["4"] = 3,
+        ["5"] = 4,
+        ["6"] = 5,
+        ["7"] = 6,
+        ["8"] = 7,
+        ["9"] = 8,
+    }
+
+    if of_items and (input_code == "ofkc") and (selected_candidate_keyval_tbl[keyvalue]) then
+        if keyvalue == "Escape" then
+            of_items = nil
+            return 1 -- kAccepted 收下此key
+        end
+
+		local selected_candidate_index = segment.selected_index
+        local index = selected_candidate_keyval_tbl[keyvalue]
+        local idx = index=="x" and selected_candidate_index or index
+        local candidateText = segment:get_candidate_at(idx).text
+        if candidateText:match('^http') then
+            local command = "open " .. candidateText
+            cmd(command)
+            of_items = nil
+            context:clear()
+            return 1 -- kAccepted 收下此key
+        end
+        of_items = nil
+        return 1 -- kAccepted 收下此key
+    end
 
 	if context:has_menu() or context:is_composing() then
-		local keyvalue = key:repr()
 		local idx = -1
 		local selected_candidate_index = segment.selected_index
 		if keyvalue == "space" then
+			idx = selected_candidate_index
+		elseif keyvalue == "Return" and (input_code == "ofkc") then
 			idx = selected_candidate_index
 		elseif keyvalue == "semicolon" then
 			idx = 1
 		elseif keyvalue == "apostrophe" then
 			idx = 2
 		end
+
 		if keyvalue == "1" then
 			idx = 0
 		elseif string.find(keyvalue, "^[2-9]$") then
@@ -86,7 +129,19 @@ function launcher.processor(key, env)
 				return 1 -- kAccepted 收下此key
 			end
 		end
+
+        if (not of_items) and (preedit_code_length == 4) and (idx >= 0) and (cmd_prefix == "of") then
+			local candidateText = segment:get_candidate_at(idx).text
+			local candidateComment = segment:get_candidate_at(idx).comment
+			if candidateText and candidateComment:match("快捷指令") then
+                of_items = app_items["Favor"][candidateText]
+                context:refresh_non_confirmed_composition() -- 刷新当前输入法候选菜单, 实现看到实时效果
+	            return 1
+            end
+        end
+
 	end
+
 	return 2
 end
 
@@ -104,6 +159,22 @@ function launcher.translator(input, seg, env)
 		cand.quality = 999
 		yield(cand)
 	end
+
+    if (not of_items) and input:match('^ofkc') then
+        for key, _ in pairs(app_items["Favor"]) do
+			local cand = Candidate("favor", seg.start, seg._end, key, "〔快捷指令〕")
+			cand.quality = 999
+			yield(cand)
+        end
+    end
+
+    if of_items and input:match('^ofkc') then
+        for _, val in pairs(of_items) do
+			local cand = Candidate("favor", seg.start, seg._end, val, "")
+			cand.quality = 999
+			yield(cand)
+        end
+    end
 end
 
 return launcher
