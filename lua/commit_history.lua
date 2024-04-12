5ee0eb68dc65c300f3ae8ee6d1943bd57d6c03e2
Shingo <boomker@users.noreply.github.com>
2024-04-12 19:42:28 +0800

    feat: 超级简拼, impro: launcher

--- lua/commit_history.lua
+++ lua/commit_history.lua
@@ -1,50 +1,47 @@
--- local puts = require("tools/debugtool")
--- ============================================================= translator
-
 local translator = {}
 
 local history_list = {}
 
 local function is_candidate_in_type(cand, type)
-	local cs = cand:get_genuines()
-	for _, c in pairs(cs) do
-		if c.type == type then
-			return true
-		end
-	end
-	return false
+    local cs = cand:get_genuines()
+    for _, c in pairs(cs) do
+        if c.type == type then
+            return true
+        end
+    end
+    return false
 end
 
 function translator.init(env)
-	local config = env.engine.schema.config
-	local history_num_max = config:get_string("history" .. "/history_num_max") or 10
-	local excluded_type = config:get_string("history" .. "/excluded_type") or {}
-	if #history_list >= tonumber(history_num_max) then
-		table.remove(history_list, 1)
-	end
-	env.notifier_commit_history = env.engine.context.commit_notifier:connect(function(ctx)
-		local cand = ctx:get_selected_candidate()
-		if cand and not is_candidate_in_type(cand, excluded_type) then
-			table.insert(history_list, cand)
-		end
-	end)
+    local config = env.engine.schema.config
+    local history_num_max = config:get_string("history" .. "/history_num_max") or 10
+    local excluded_type = config:get_string("history" .. "/excluded_type") or {}
+    if #history_list >= tonumber(history_num_max) then
+        table.remove(history_list, 1)
+    end
+    env.notifier_commit_history = env.engine.context.commit_notifier:connect(function(ctx)
+        local cand = ctx:get_selected_candidate()
+        if cand and not is_candidate_in_type(cand, excluded_type) then
+            table.insert(history_list, cand)
+        end
+    end)
 end
 
 function translator.fini(env)
-	env.notifier_commit_history:disconnect()
+    env.notifier_commit_history:disconnect()
 end
 
 ---@diagnostic disable-next-line: unused-local
 function translator.func(input, seg, env)
-	if seg:has_tag("history") or input == "hisz" then
-		for i = #history_list, 1, -1 do
-			local cand = Candidate("history", seg.start, seg._end, history_list[i].text, "history")
-			local cand_uniq = UniquifiedCandidate(cand, cand.type, cand.text, cand.comment)
-			yield(cand_uniq)
-		end
-	end
+    if seg:has_tag("history") or input == "hisz" then
+        for i = #history_list, 1, -1 do
+            local cand = Candidate("history", seg.start, seg._end, history_list[i].text, "history")
+            local cand_uniq = UniquifiedCandidate(cand, cand.type, cand.text, cand.comment)
+            yield(cand_uniq)
+        end
+    end
 end
 
 return {
-	translator = { init = translator.init, func = translator.func, fini = translator.fini },
+    translator = { init = translator.init, func = translator.func, fini = translator.fini },
 }
