6d47d74a9a338abd21b713d9435c696bb2eb635f
Shingo <boomker@users.noreply.github.com>
2024-04-13 02:25:36 +0800

    improve: lua

--- lua/date_time.lua
+++ lua/date_time.lua
@@ -16,232 +16,232 @@
 local tool = require("tools/number_to_cn")
 
 local conf = {
-	day_en_st = {
-		"1st",
-		"2nd",
-		"3th",
-		"4th",
-		"5th",
-		"6th",
-		"7th",
-		"8th",
-		"9th",
-		"10th",
-		"11th",
-		"12th",
-		"13th",
-		"14th",
-		"15th",
-		"16th",
-		"17th",
-		"18th",
-		"19th",
-		"20th",
-		"21th",
-		"22th",
-		"23th",
-		"24th",
-		"25th",
-		"26th",
-		"27th",
-		"28th",
-		"29th",
-		"30th",
-		"31th",
-	},
-	week_cn = {
-		"星期日",
-		"星期一",
-		"星期二",
-		"星期三",
-		"星期四",
-		"星期五",
-		"星期六",
-	},
-	week_en = {
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	},
-	week_en_st = { "Sun.", "Mon.", "Tues.", "Wed.", "Thur.", "Fri.", "Sat." },
-	month_en = {
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	},
-	month_en_st = {
-		"Jan.",
-		"Feb.",
-		"Mar.",
-		"Apr.",
-		"May.",
-		"Jun.",
-		"Jul.",
-		"Aug.",
-		"Sept.",
-		"Oct.",
-		"Nov.",
-		"Dec.",
-	},
+    day_en_st = {
+        "1st",
+        "2nd",
+        "3th",
+        "4th",
+        "5th",
+        "6th",
+        "7th",
+        "8th",
+        "9th",
+        "10th",
+        "11th",
+        "12th",
+        "13th",
+        "14th",
+        "15th",
+        "16th",
+        "17th",
+        "18th",
+        "19th",
+        "20th",
+        "21th",
+        "22th",
+        "23th",
+        "24th",
+        "25th",
+        "26th",
+        "27th",
+        "28th",
+        "29th",
+        "30th",
+        "31th",
+    },
+    week_cn = {
+        "星期日",
+        "星期一",
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六",
+    },
+    week_en = {
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    },
+    week_en_st = { "Sun.", "Mon.", "Tues.", "Wed.", "Thur.", "Fri.", "Sat." },
+    month_en = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    },
+    month_en_st = {
+        "Jan.",
+        "Feb.",
+        "Mar.",
+        "Apr.",
+        "May.",
+        "Jun.",
+        "Jul.",
+        "Aug.",
+        "Sept.",
+        "Oct.",
+        "Nov.",
+        "Dec.",
+    },
 }
 
 conf.pattern_date = {
-	"{year}年{month}月{day}日", -- 2022年09月05日
-	"{year}{month}{day}", -- 20220905
-	"{year}/{month}/{day}", -- 2022/09/05
-	"{year}.{month}.{day}", -- 2022.09.05
-	"{year}-{month}-{day}", -- 2022-09-05
-	"{year.number_cn}年{month.arith.number_cn}月{day.arith.number_cn}日", -- 二〇二二年十一月二十五日
-	"{month_en} {day_en_st},{year}", -- September 05th,2022
-	"{month_en_st} {day_en_st},{year}", -- Sept.05th,2022
+    "{year}年{month}月{day}日", -- 2022年09月05日
+    "{year}{month}{day}", -- 20220905
+    "{year}/{month}/{day}", -- 2022/09/05
+    "{year}.{month}.{day}", -- 2022.09.05
+    "{year}-{month}-{day}", -- 2022-09-05
+    "{year.number_cn}年{month.arith.number_cn}月{day.arith.number_cn}日", -- 二〇二二年十一月二十五日
+    "{month_en} {day_en_st},{year}", -- September 05th,2022
+    "{month_en_st} {day_en_st},{year}", -- Sept.05th,2022
 }
 
 conf.pattern_today = {
-	"{year}{month}{day}", -- 20220905
-	"{year}{month}{day}{hour}{min}", -- 202209051836
-	"{year}{month}{day}{hour}{min}{sec}", -- 20220905183658
-	"{year}-{month}-{day} {hour}:{min}:{sec}", -- 2022-09-05 18:36:58
+    "{year}{month}{day}",                   -- 20220905
+    "{year}{month}{day}{hour}{min}",        -- 202209051836
+    "{year}{month}{day}{hour}{min}{sec}",   -- 20220905183658
+    "{year}-{month}-{day} {hour}:{min}:{sec}", -- 2022-09-05 18:36:58
 }
 
 conf.pattern_week = {
-	"{week_cn}", -- 星期一
-	"{week_en}", -- Monday
-	"{week_en_st}", -- Mon.
+    "{week_cn}", -- 星期一
+    "{week_en}", -- Monday
+    "{week_en_st}", -- Mon.
 }
 
 conf.pattern_time = {
-	"{hour}:{min}", -- 18:36
-	"{hour}:{min}:{sec}", -- 18:36:58
-	"{hour}点{min}分", -- 18点37分
-	"{hour}点{min}分{sec}秒", -- 18点37分06秒
+    "{hour}:{min}", -- 18:36
+    "{hour}:{min}:{sec}", -- 18:36:58
+    "{hour}点{min}分", -- 18点37分
+    "{hour}点{min}分{sec}秒", -- 18点37分06秒
 }
 
 local function getTimeStr(str)
-	while true do
-		local pattern = string.match(str, "%b{}")
-		local replace_index = nil
-		local replace_value = ""
-		local flag = false -- 处理链，以flag基准，判断是否处理下去
+    while true do
+        local pattern = string.match(str, "%b{}")
+        local replace_index = nil
+        local replace_value = ""
+        local flag = false -- 处理链，以flag基准，判断是否处理下去
 
-		if pattern then
-			if string.find(pattern, "^{year") then
-				replace_index = os.date("%Y") -- 年
-			elseif string.find(pattern, "^{month") then
-				replace_index = os.date("%m") -- 月
-			elseif string.find(pattern, "^{day") then
-				replace_index = os.date("%d") -- 日
-			elseif string.find(pattern, "^{week") then
-				replace_index = os.date("%w") + 1 -- 星期
-			elseif string.find(pattern, "^{hour") then
-				replace_index = os.date("%H") -- 时
-			elseif string.find(pattern, "^{min") then
-				replace_index = os.date("%M") -- 分
-			elseif string.find(pattern, "^{sec") then
-				replace_index = os.date("%S") -- 秒
-			end
+        if pattern then
+            if string.find(pattern, "^{year") then
+                replace_index = os.date("%Y") -- 年
+            elseif string.find(pattern, "^{month") then
+                replace_index = os.date("%m") -- 月
+            elseif string.find(pattern, "^{day") then
+                replace_index = os.date("%d") -- 日
+            elseif string.find(pattern, "^{week") then
+                replace_index = os.date("%w") + 1 -- 星期
+            elseif string.find(pattern, "^{hour") then
+                replace_index = os.date("%H") -- 时
+            elseif string.find(pattern, "^{min") then
+                replace_index = os.date("%M") -- 分
+            elseif string.find(pattern, "^{sec") then
+                replace_index = os.date("%S") -- 秒
+            end
 
-			if replace_index then
-				local _ni = tonumber(replace_index)
-				-- 值：转换为中文，e.g. 0=>〇，1=>一，...
-				if (not flag) and string.find(pattern, ".number_cn}$") then
-					if string.find(pattern, ".arith.number_cn}$") then
-						-- 21 => 二十一
-						replace_value = tool.convert_arab_to_chinese(_ni)
-					else
-						-- 21 => 二一
-						local _nis = tostring(_ni)
-						for i = 1, _nis:len() do
-							local _ni_digit = tonumber(_nis:sub(i, i))
-							replace_value = replace_value .. tool.convert_arab_to_chinese(_ni_digit)
-						end
-					end
-					replace_value = string.gsub(replace_value, "零", "〇")
-					flag = true
-				end
+            if replace_index then
+                local _ni = tonumber(replace_index)
+                -- 值：转换为中文，e.g. 0=>〇，1=>一，...
+                if (not flag) and string.find(pattern, ".number_cn}$") then
+                    if string.find(pattern, ".arith.number_cn}$") then
+                        -- 21 => 二十一
+                        replace_value = tool.convert_arab_to_chinese(_ni)
+                    else
+                        -- 21 => 二一
+                        local _nis = tostring(_ni)
+                        for i = 1, _nis:len() do
+                            local _ni_digit = tonumber(_nis:sub(i, i))
+                            replace_value = replace_value .. tool.convert_arab_to_chinese(_ni_digit)
+                        end
+                    end
+                    replace_value = string.gsub(replace_value, "零", "〇")
+                    flag = true
+                end
 
-				-- 值：根据conf的prop转换
-				if not flag then
-					local prop_name = string.match(pattern, "{(.+)}")
-					local prop = conf[prop_name]
-					if prop then
-						replace_value = prop[_ni]
-						flag = true
-					end
-				end
-			end
+                -- 值：根据conf的prop转换
+                if not flag then
+                    local prop_name = string.match(pattern, "{(.+)}")
+                    local prop = conf[prop_name]
+                    if prop then
+                        replace_value = prop[_ni]
+                        flag = true
+                    end
+                end
+            end
 
-			if not flag then -- 默认值
-				replace_value = tostring(replace_index)
-			end
-			str = replace_value and string.gsub(str, pattern, replace_value)
-		else
-			break
-		end
-	end
-	return str
+            if not flag then -- 默认值
+                replace_value = tostring(replace_index)
+            end
+            str = replace_value and string.gsub(str, pattern, replace_value)
+        else
+            break
+        end
+    end
+    return str
 end
 
 local translator = {}
 
 ---@diagnostic disable-next-line: unused-local
 function translator.func(input, seg, env)
-	if seg:has_tag("date") or (input == "date" or input == "rq") then
-		-- 日期
-		local tip = "〔日期〕"
-		for _, v in ipairs(conf.pattern_date) do
-			local comment = getTimeStr(v)
-			local cand = Candidate("date", seg.start, seg._end, comment, tip)
-			cand.preedit = string.sub(input, seg._start + 1, seg._end)
-			cand.quality = 999
-			yield(cand)
-		end
-	end
-	if seg:has_tag("week") or input == "week" then
-		-- 星期
-		local tip = "〔星期〕"
-		for _, v in ipairs(conf.pattern_week) do
-			local comment = getTimeStr(v)
-			local cand = Candidate("week", seg.start, seg._end, comment, tip)
-			cand.preedit = string.sub(input, seg._start + 1, seg._end)
-			cand.quality = 999
-			yield(cand)
-		end
-	end
-	if seg:has_tag("time") or input == "time" or input == "wt" then
-		-- 时间
-		local tip = "〔时间〕"
-		for _, v in ipairs(conf.pattern_time) do
-			local comment = getTimeStr(v)
-			local cand = Candidate("time", seg.start, seg._end, comment, tip)
-			cand.preedit = string.sub(input, seg._start + 1, seg._end)
-			cand.quality = 999
-			yield(cand)
-		end
-	end
-	if input == "dt" or input == "today" then
-		-- 日期
-		local tip = "〔日期〕"
-		for _, v in ipairs(conf.pattern_today) do
-			local comment = getTimeStr(v)
-			local cand = Candidate("today", seg.start, seg._end, comment, tip)
-			cand.preedit = string.sub(input, seg._start + 1, seg._end)
-			cand.quality = 1
-			yield(cand)
-		end
-	end
+    if seg:has_tag("date") or (input == "date" or input == "rq") then
+        -- 日期
+        local tip = "〔日期〕"
+        for _, v in ipairs(conf.pattern_date) do
+            local comment = getTimeStr(v)
+            local cand = Candidate("date", seg.start, seg._end, comment, tip)
+            cand.preedit = string.sub(input, seg._start + 1, seg._end)
+            cand.quality = 999
+            yield(cand)
+        end
+    end
+    if seg:has_tag("week") or input == "week" then
+        -- 星期
+        local tip = "〔星期〕"
+        for _, v in ipairs(conf.pattern_week) do
+            local comment = getTimeStr(v)
+            local cand = Candidate("week", seg.start, seg._end, comment, tip)
+            cand.preedit = string.sub(input, seg._start + 1, seg._end)
+            cand.quality = 999
+            yield(cand)
+        end
+    end
+    if seg:has_tag("time") or input == "time" or input == "wt" then
+        -- 时间
+        local tip = "〔时间〕"
+        for _, v in ipairs(conf.pattern_time) do
+            local comment = getTimeStr(v)
+            local cand = Candidate("time", seg.start, seg._end, comment, tip)
+            cand.preedit = string.sub(input, seg._start + 1, seg._end)
+            cand.quality = 999
+            yield(cand)
+        end
+    end
+    if input == "dt" or input == "today" then
+        -- 日期
+        local tip = "〔日期〕"
+        for _, v in ipairs(conf.pattern_today) do
+            local comment = getTimeStr(v)
+            local cand = Candidate("today", seg.start, seg._end, comment, tip)
+            cand.preedit = string.sub(input, seg._start + 1, seg._end)
+            cand.quality = 999
+            yield(cand)
+        end
+    end
 end
 
 return { translator = translator }
--- lua/long_word_up.lua
+++ lua/long_word_up.lua
@@ -1,57 +1,57 @@
-
 local function long_word_up(input, env)
-	local engine = env.engine
-	local config = engine.schema.config
+    local engine = env.engine
+    local context = engine.context
+    local config = engine.schema.config
 
-	local cands = {}
-	local longWord_cands = {}
-	-- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
-	local prev_word_length = 0
-	-- 记录筛选了多少个汉语词条(只提升1个词的权重)
-	local pickup_count = 1
-	local idx = config:get_int("long_word_up_config/idx") or 3
+    local cands = {}
+    local longWord_cands = {}
+    -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
+    local prev_word_length = 0
+    -- 记录筛选了多少个汉语词条(只提升1个词的权重)
+    local pickup_count = 1
+    local idx = config:get_int("long_word_up_config/idx") or 3
 
-	local preedit_code = env.engine.context:get_commit_text()
+    local preedit_code = context:get_commit_text():gsub("[ ']", "")
     local preedit_length = preedit_code:len()
-	for cand in input:iter() do
+    for cand in input:iter() do
         local cand_text = cand.text:gsub(" ", "")
-		local cand_length = utf8.len(cand_text)
-		if (cand.quality > 9) or (idx > 1) then
-			prev_word_length = cand_length or 0
-			idx = idx - 1
-			yield(cand)
-		elseif
-			(cand_length > prev_word_length)
-			and (cand_length >= 3)
-			and (preedit_length > 3)
-			and (pickup_count >= 1)
-			and (#cand.comment < 3)
+        local cand_length = utf8.len(cand_text)
+        if (cand.quality > 9) or (idx > 1) then
+            prev_word_length = cand_length or 0
+            idx = idx - 1
+            yield(cand)
+        elseif
+            (cand_length > prev_word_length)
+            and (cand_length >= 3)
+            and (preedit_length > 3)
+            and (pickup_count >= 1)
+            and (#cand.comment < 3)
             and (cand_length <= preedit_length)
-			and (not cand_text:match("[a-zA-Z]"))
-			and (cand:get_dynamic_type() ~= "Shadow")
-		then
-			local cand_uniq = UniquifiedCandidate(cand, "LongWordUp", cand_text, cand.comment)
-			yield(cand_uniq)
-			pickup_count = pickup_count - 1
-		else
-			if ((utf8.len(cand_text) / #preedit_code) <= 1.5) or (cand.quality > 9) then
-				table.insert(cands, cand)
-			else
-				table.insert(longWord_cands, cand)
-			end
-		end
+            and (not cand_text:match("[a-zA-Z]"))
+            and (cand:get_dynamic_type() ~= "Shadow")
+        then
+            local cand_uniq = UniquifiedCandidate(cand, "LongWordUp", cand_text, cand.comment)
+            yield(cand_uniq)
+            pickup_count = pickup_count - 1
+        else
+            if ((utf8.len(cand_text) / #preedit_code) <= 1.5) or (cand.quality > 9) then
+                table.insert(cands, cand)
+            else
+                table.insert(longWord_cands, cand)
+            end
+        end
 
-		if #cands > 80 then
-			break
-		end
-	end
+        if #cands > 80 then
+            break
+        end
+    end
 
-	for _, cand in ipairs(cands) do
-		yield(cand)
-	end
-	for _, long_cand in ipairs(longWord_cands) do
-		yield(long_cand)
-	end
+    for _, cand in ipairs(cands) do
+        yield(cand)
+    end
+    for _, long_cand in ipairs(longWord_cands) do
+        yield(long_cand)
+    end
 end
 
 return { filter = long_word_up }
