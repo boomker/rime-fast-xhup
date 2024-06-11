985ddddce0973917a03a4ffe6e5053e04ff69284
Shingo <boomker@users.noreply.github.com>
2024-06-11 16:31:28 +0800

    Update reduce_freq_words.lua

--- lua/cold_word_record/reduce_freq_words.lua
+++ lua/cold_word_record/reduce_freq_words.lua
@@ -1,16 +1,15 @@
-local turndown_freq_words =
-{ 	["爬船"] = { "pair", },
-	["结社"] = { "jpue", },
-	["托德斯"] = { "todes", },
-	["恩特"] = { "ente", },
-	["石菖蒲"] = { "uiihp", },
-	["捉对"] = { "vodv", },
-	["爬松"] = { "pass", },
-	["郑损"] = { "vgsy", },
-	["佛莱尔"] = { "folder", },
-	["官婢"] = { "grbi", },
-	["狸奴下"] = { "linux", },
-	["王二娥"] = { "where", },
-	["目无余子"] = { "mwyz", },
+local reduce_freq_words =
+{
+    ["捉对"] = { "vodv", },
+    ["颜射"] = { "yjue", },
+    ["托德斯"] = { "todes", },
+    ["石菖蒲"] = { "uiihp", },
+    ["爬松"] = { "pass", },
+    ["爬船"] = { "pair", },
+    ["目无余子"] = { "mwyz", },
+    ["结社"] = { "jpue", },
+    ["官婢"] = { "grbi", },
+    ["佛莱尔"] = { "folder", },
+    ["恩特"] = { "ente", },
 }
-return turndown_freq_words
+return reduce_freq_words
