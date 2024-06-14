33f7585f27a6599d63a9c32175b4f3ea552e5df0
Shingo <boomker@users.noreply.github.com>
2024-06-15 02:18:46 +0800

    update dict and opti lua

--- lua/select_char.lua
+++ lua/select_char.lua
@@ -28,8 +28,9 @@ local function reset_cand_property(env)
     local context = env.engine.context
     context:set_property("prev_cand_is_null", "0")
     context:set_property("prev_cand_is_word", "0")
-    context:set_property("prev_cand_is_hanzi", "1")
+    context:set_property("prev_cand_is_hanzi", "0")
     context:set_property("prev_cand_is_preedit", "0")
+    context:set_property("prev_commit_is_comma", "0")
 end
 
 function P.init(env)
