8168da40a4f1a0b9e2029c9ec21f45602f46e852
Shingo <boomker@users.noreply.github.com>
2024-06-22 23:42:08 +0800

    opti lua

--- lua/fly_fixed.lua
+++ lua/fly_fixed.lua
@@ -1,30 +1,31 @@
 require("tools/string")
-local fly_fixed = {}
+-- local logger = require("tools/logger")
+local F = {}
 
 local function last_character(s)
     return string.utf8_sub(s, -1, -1)
 end
 
-function fly_fixed.init(env)
+function F.init(env)
     local config = env.engine.schema.config
     env.pin_mark = config:get_string("pin_word/comment_mark") or "🔝"
     local schema_id = config:get_string("translator/dictionary") -- 多方案共用字典取主方案名称
     env.reversedb = ReverseLookup(schema_id)
-    collectgarbage("step")
 end
 
-function fly_fixed.func(input, env)
+function F.func(input, env)
     local cands = {}
     local cand_drop = false
     local context = env.engine.context
     local preedit_code = context.input:gsub(" ", "")
     for cand in input:iter() do
         local cand_text = cand.text:gsub(" ", "")
-        if
+        local cand_type = cand:get_dynamic_type()
+        if -- 将非原始小鹤双拼编码规则产生的候选词条结果降频, 置于最后输出
             (cand.type ~= "user_table")
+            and (cand_type ~= "Shadow")
             and (not cand_text:match("[a-zA-Z]"))
             and (not preedit_code:match("[%u%p]"))
-            and (not cand:get_dynamic_type() == "Shadow")
             and (string.utf8_len(cand_text) <= #preedit_code)
             and ((#preedit_code % 2 ~= 0) and (#preedit_code <= 7))
             and (not cand.comment:match("^" .. env.pin_mark .. "$"))
@@ -37,22 +38,25 @@ function fly_fixed.func(input, env)
             else
                 table.insert(cands, cand)
             end
-        elseif cand_text:match("<br>") then
+        elseif cand_text:match("<br>") then -- 词条有<br>标签, 将其转为换行符
             local candTxt = cand_text:gsub("<br>", "\r\t")
             yield(Candidate("word", cand.start, cand._end, candTxt, ""))
-        elseif
+        elseif -- 丢弃一些候选结果
+        -- 开头大写的预编辑编码, 去掉只有单字母的候选
             (preedit_code:match("^[%u][%a]+") and cand_text:match("^[A-Z]$"))
             or (
+            -- V模式下, 过滤掉中英混合词条
                 preedit_code:match("^[%u][%a]+$")
                 and cand_text:find("([\228-\233][\128-\191]-)")
             ) or (
+            -- 辅码筛字时, 过滤掉 emoji
                 preedit_code:match("^%l+[%[`]%l?%l?$")
                 and (cand:get_dynamic_type() == "Shadow")
-            )
-            or (
+            ) or (
+            -- 候选词长度大于预编辑长度
                 (cand.type == "completion") and
                 (not cand_text:match("[%a%p]")) and
-                (string.utf8_len(cand_text) - #preedit_code > 2)
+                (string.utf8_len(cand_text) - #preedit_code > 1)
             )
         then
             cand_drop = true
@@ -60,14 +64,15 @@ function fly_fixed.func(input, env)
             yield(cand)
         end
 
-        if #cands >= 80 then break end
+        if #cands >= 100 then break end
     end
 
-    for _, cand in ipairs(cands) do
-        yield(cand)
-    end
+    for _, cand in ipairs(cands) do yield(cand) end
     -- GC
-    if math.random() < 0.1 then collectgarbage() end
+    if cand_drop then
+        collectgarbage()
+        cand_drop = false
+    end
 end
 
-return { filter = { init = fly_fixed.init, func = fly_fixed.func } }
+return F
