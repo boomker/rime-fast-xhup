986058c318143ba68e4cbb2491d530208c818335
Shingo <boomker@users.noreply.github.com>
2024-07-22 09:51:54 +0800

    update .gitignore and cold_word_drop

---
+++ lua/cold_word_records/reduce_freq_words.lua
@@ -0,0 +1,13 @@
+local reduce_freq_words =
+{
+    ["捉对"] = { "vodv", },
+    ["颜射"] = { "yjue", },
+    ["托德斯"] = { "todes", },
+    ["爬船"] = { "pair", },
+    ["目无余子"] = { "mwyz", },
+    ["结社"] = { "jpue", },
+    ["官婢"] = { "grbi", },
+    ["佛莱尔"] = { "folder", },
+    ["恩特"] = { "ente", },
+}
+return reduce_freq_words
