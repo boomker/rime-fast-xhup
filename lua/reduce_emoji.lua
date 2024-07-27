795240d368cfab92a21bba0737df71b6cd8839cf
Shingo <boomker@users.noreply.github.com>
2024-07-28 02:33:40 +0800

    improve: fly_fixed.lua, 删除脏词

--- lua/reduce_emoji.lua
+++ lua/reduce_emoji.lua
@@ -3,10 +3,10 @@ local F = {}
 function F.init(env)
     local engine = env.engine
     local config = engine.schema.config
-    -- env.mem = Memory(engine, engine.schema)
     env.emoji_pos = config:get_int("emoji_reduce/idx") or 6
     env.pin_mark = config:get_string("pin_word/comment_mark") or "🔝"
     --[[
+    -- env.mem = Memory(engine, engine.schema)
     env.notifier_commit = env.engine.context.commit_notifier:connect(function(ctx)
         local cand = ctx:get_selected_candidate()
         if (cand:get_dynamic_type() == "Shadow") then
@@ -28,40 +28,36 @@ function F.init(env)
 end
 
 function F.func(input, env)
-    local emoji_cands  = {}
-    local other_cands  = {}
-    local top_cand_cnt = 0
-    local engine       = env.engine
-    local emoji_toggle = engine.context:get_option("emoji")
-    local wechat_flag  = engine.context:get_option("wechat_flag")
-    local preedit_code = engine.context.input:gsub(" ", "")
+    local engine         = env.engine
+    local preedit_code   = engine.context.input:gsub(" ", "")
+    local emoji_toggle   = engine.context:get_option("emoji")
+    local wechat_flag    = engine.context:get_option("wechat_flag")
+    local emoji_cands    = {}
+    local other_cands    = {}
+    local top_cand_count = 0
 
     for cand in input:iter() do
-        if (top_cand_cnt <= env.emoji_pos) then
+        if (top_cand_count <= env.emoji_pos) then
+            if cand.comment:match(env.pin_mark) then
+                yield(cand)
+            end
             if
                 emoji_toggle
                 and (cand:get_dynamic_type() == "Shadow")
-                and (not cand.comment:match(env.pin_mark))
                 and (not preedit_code:match("^%l+[%[`]%l?%l?$"))
-                and (not (
-                    cand.text:find("([\228-\233][\128-\191]-)")
-                    and cand.text:lower():match("^" .. preedit_code)
-                ))
             then
                 table.insert(emoji_cands, cand)
             elseif (cand:get_dynamic_type() == "Shadow")
                 and (
-                    cand.comment:match(env.pin_mark) or (
-                        cand.text:find("([\228-\233][\128-\191]-)")
-                        and (cand.text:lower():match("^" .. preedit_code))
-                    )
+                    cand.text:find("([\228-\233][\128-\191]-)")
+                    and (cand.text:lower():match("^" .. preedit_code))
                 )
             then
                 yield(cand)
-            elseif (cand:get_dynamic_type() ~= "Shadow") then
+            else
                 yield(cand)
             end
-            top_cand_cnt = top_cand_cnt + 1
+            top_cand_count = top_cand_count + 1
         else
             table.insert(other_cands, cand)
         end
