e3f9f7461b259c7e0d4bf8b255aa9fcf37ab4e59
Shingo <boomker@users.noreply.github.com>
2024-08-07 11:35:46 +0800

    update dict

---
+++ scripts/purge_words.sh
@@ -0,0 +1,5 @@
+sed -i '/糖分手/d' cn_dicts/flypy_sogou.dict.yaml
+sed -i '/中国第金/d' cn_dicts/flypy_sogou.dict.yaml
+sed -i '/崔宸曦第/d' cn_dicts/flypy_sogou.dict.yaml
+sed -i '/樊正东/s/正/振/1' cn_dicts/flypy_sogou.dict.yaml
+sed -i 's/孙银沙/孙颖莎/1' cn_dicts/flypy_sogou.dict.yaml
