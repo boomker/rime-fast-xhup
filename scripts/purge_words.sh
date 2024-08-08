84867e54ee7984435720e33517bdce552a600921
Shingo <boomker@users.noreply.github.com>
2024-08-08 20:25:17 +0800

    feat: 支持一键调用Easydict翻译高亮候选字词

--- scripts/purge_words.sh
+++ scripts/purge_words.sh
@@ -1,5 +1,6 @@
 sed -i '/糖分手/d' cn_dicts/flypy_sogou.dict.yaml
 sed -i '/中国第金/d' cn_dicts/flypy_sogou.dict.yaml
 sed -i '/崔宸曦第/d' cn_dicts/flypy_sogou.dict.yaml
+sed -i '/积分夺宝卷/d' cn_dicts/flypy_sogou.dict.yaml
 sed -i '/樊正东/s/正/振/1' cn_dicts/flypy_sogou.dict.yaml
 sed -i 's/孙银沙/孙颖莎/1' cn_dicts/flypy_sogou.dict.yaml
