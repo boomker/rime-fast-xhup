92d360f1d59cb9c1dbb9b54994e4d989de120dcd
Shingo <boomker@users.noreply.github.com>
2024-08-22 01:22:31 +0800

    update dict

--- lua/fly_fixed.lua
+++ lua/fly_fixed.lua
@@ -13,19 +13,23 @@ function F.init(env)
 end
 
 function F.func(input, env)
-    local cands = {}
-    local cand_drop = false
+	local drop_cand = false
     local cmp_cand_count = 0
     local rdb = env.reversedb
+	local untimely_emojis = {}
+    local low_priority_cands = {}
     local context = env.engine.context
     local preedit_code = context.input:gsub(" ", "")
     local confirmed_syllable_len = math.floor(#preedit_code / 2)
     for cand in input:iter() do
-        local cand_text = cand.text:gsub(" ", "")
         local cand_type = cand:get_dynamic_type()
-        if                          -- 丢弃一些候选结果
+        local cand_text = cand.text:gsub(" ", "")
+        local cand_comment = cand.comment:gsub("[〔〕]", "")
+        if cand.comment:match("^" .. env.pin_mark .. "$") then
+            yield(cand)
+        elseif                      -- 丢弃一些候选结果
             cand_text:match("<br>") -- 去除'<br>'重复候选
-            -- 开头大写的预编辑编码, 去掉只有单字母的候选
+            -- 开头大写的输入编码, 去掉只有单字母的候选
             or (
                 preedit_code:match("^[%u][%a]+")
                 and cand_text:match("^[A-Z]$")
@@ -43,9 +47,10 @@ function F.func(input, env)
                 (not preedit_code:match("%p")) and
                 (not cand_text:match("[%a%p]")) and
                 (utf8.len(cand_text) - confirmed_syllable_len > 2)
-            )
+            -- 候选词是 不匹配的 Emoji 时则丢弃
+            ) or (table.find_index(untimely_emojis, cand_comment))
         then
-            cand_drop = true
+			drop_cand = true
         elseif -- 候选词长度超出预确认音节长度 1 个以上的候选
             (cand.type == "completion") and
             (not preedit_code:match("%p")) and
@@ -54,7 +59,7 @@ function F.func(input, env)
         then
             cmp_cand_count = cmp_cand_count + 1
             if cmp_cand_count >= 3 then
-                cand_drop = true
+				drop_cand = true
             else
                 yield(cand)
             end
@@ -66,7 +71,6 @@ function F.func(input, env)
             and (utf8.len(cand_text) <= #preedit_code)
             and (utf8.len(cand_text) >= confirmed_syllable_len)
             and ((#preedit_code % 2 ~= 0) and (#preedit_code <= 7))
-            and (not cand.comment:match("^" .. env.pin_mark .. "$"))
         then
             local last_char = last_character(cand_text)
             local preedit_last_code = preedit_code:sub(-1, -1)
@@ -74,17 +78,18 @@ function F.func(input, env)
             if yin_code and (yin_code:match(preedit_last_code)) then
                 yield(cand)
             else
-                table.insert(cands, cand)
+                table.insert(low_priority_cands, cand)
+				table.insert(untimely_emojis, cand.text)
             end
         else
             yield(cand)
         end
 
-        if #cands >= 100 then break end
+        if #low_priority_cands >= 120 then break end
     end
 
-    if cand_drop then cand_drop = false end
-    for _, cand in ipairs(cands) do yield(cand) end
+	if drop_cand then drop_cand = false end
+    for _, cand in ipairs(low_priority_cands) do yield(cand) end
     -- GC
     -- collectgarbage()
 end
