0d2adb30632c3bfaafe4b767111691ca584600ed
Shingo <boomker@users.noreply.github.com>
2024-08-22 12:14:57 +0800

    reduce .git object and Re-push missing files

--- lua/pin_word.lua
+++ lua/pin_word.lua
@@ -155,7 +155,7 @@ function F.func(input, env)
         else
             table.insert(other_cands, cand)
         end
-        if #other_cands >= 100 then break end
+        if #other_cands >= 120 then break end
     end
 
     if #pin_cands > 0 then
