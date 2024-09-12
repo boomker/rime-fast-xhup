a25c459a48420fc9547a94a5e07c2f3c59c2246f
Shingo <boomker@users.noreply.github.com>
2024-09-12 23:57:14 +0800

    update dict

--- lua/en_custom.lua
+++ lua/en_custom.lua
@@ -25,9 +25,8 @@ function T.init(env)
 end
 
 function T.func(input, seg, env)
-    if input:match("^%a[%a%p]+%]$") then           -- 输入末尾必须是 `]`
+    if input:match("^%a[%a%p]+\\$") then           -- 输入末尾必须是 `\`
         local inp = input:sub(1, -2):gsub(" ", "") -- -3对应两个末尾符号, -2对应一个
-        -- local record = inp .. "\t" .. inp:gsub("[^%a-_/]+", ""):lower() .. "\t100000"
         local record = inp .. "\t" .. inp:gsub("]+", "") .. "\t100000"
         if not user_dict_exist(record, env.dict_path) then
             yield(Candidate("en_custom", seg.start, seg._end, inp, "✅"))
