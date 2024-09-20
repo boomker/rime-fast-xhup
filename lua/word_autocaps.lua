58821cb861ed713a142cebd8b5348b5084863573
Shingo <boomker@users.noreply.github.com>
2024-09-20 13:43:56 +0800

    opti: lua scripts and dicts

--- lua/word_autocaps.lua
+++ lua/word_autocaps.lua
@@ -1,7 +1,7 @@
 -- 输入的内容大写前2个字符，自动转小写词条为全词大写；大写第一个字符，自动转写小写词条为首字母大写
 
 local function autocap_filter(input, env)
-    local u_cands = {}
+	local u_cands = {}
 	for cand in input:iter() do
 		local text = cand.text
 		local preedit_code = env.engine.context:get_commit_text()
@@ -11,12 +11,12 @@ local function autocap_filter(input, env)
 				yield(cand_2)
 			else
 				local cand_u = Candidate("cap", 0, string.len(preedit_code), string.upper(text), "+AU")
-                table.insert(u_cands, cand_u)
+				table.insert(u_cands, cand_u)
 			end
 		elseif string.find(text, "^%l+$") and string.find(preedit_code, "^%u+") then
 			local suffix = string.sub(text, string.len(preedit_code) + 1)
 			local cand_t = Candidate("cap", 0, string.len(preedit_code), preedit_code .. suffix, "~AT")
-            table.insert(u_cands, cand_t)
+			table.insert(u_cands, cand_t)
 		else
 			yield(cand)
 		end
@@ -24,14 +24,21 @@ local function autocap_filter(input, env)
         if #u_cands >= 120 then break end
 	end
 
-    for _, cand in ipairs(u_cands) do yield(cand) end
+	for _, cand in ipairs(u_cands) do yield(cand) end
 end
 
 ---@diagnostic disable-next-line: unused-local
 local function autocap_translator(input, seg, env)
-	if string.match(input, "^%u%u%l+") then
-		local cand = Candidate("word_caps", seg.start, seg._end, string.upper(input), "~AU")
+	if input:match("^%u%l%l?%l?%l?%l?%l?%l?$") then
+		local cand = Candidate("Word", seg.start, seg._end, input, "")
+		cand.quality = 999
 		yield(cand)
+	elseif input:match("^%u%u%a+") then
+		yield(Candidate("word_caps", seg.start, seg._end, input:upper(), "~AU"))
+	elseif input:match("^%u%l+%u%a?") then
+		yield(Candidate("en_custom", seg.start, seg._end, input, "~AC"))
+	elseif input:match("^%a+[.:_-]%/?%/?%/?%a+") then
+		yield(Candidate("en_custom", seg.start, seg._end, input, "~AC"))
 	end
 end
 
