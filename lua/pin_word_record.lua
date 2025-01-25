b47367cff5678dd8c6c5a6b618e06f921511a539
Shingo <boomker@users.noreply.github.com>
2025-01-25 18:03:11 +0800

    add pin_word_record.lua via upload

---
+++ lua/pin_word_record.lua
@@ -0,0 +1,9 @@
+local pin_word_records = {
+    ["ap"] = { "安排" },
+    ["api"] = { "API" },
+    ["ah"] = { "帮", "昂" },
+    ["ba"] = { "吧", "把" },
+    ["anato"] = { "anatomy" },
+    ["back"] = { "back", "backup", "backend" },
+}
+return pin_word_records
