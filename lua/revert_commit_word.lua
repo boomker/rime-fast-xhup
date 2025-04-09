c05c5773631650f39bef2ffc38c9c9c4e30b2b78
Shingo <boomker@users.noreply.github.com>
2025-04-09 17:15:05 +0800

    chore: update lua, emoji

--- lua/revert_commit_word.lua
+++ lua/revert_commit_word.lua
@@ -1,22 +1,16 @@
 -- https://github.com/qiuyue0/rime-lua-sendKeyCode
 -- 文件名不能变，只能是sendKeyCode.so或dll
+
 local T = {}
-local SendKeyCode = require("sendKeyCode")
+require("lib/rime_helper")
 
-local function getOS()
-    local osname
-    local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
-    if fh then
-        osname = fh:read()
-    end
-    return osname or ""
-end
-
-if getOS() == "Darwin" then
+if detect_os():lower() == "macos" then
     package.cpath = os.getenv("HOME") .. "/Library/Rime/lua/lib/?.so;" .. package.cpath
-elseif getOS() == "Linux" then
+elseif detect_os():lower() == "linux" then
     package.cpath = os.getenv("HOME") .. "/.config/Rime/lua/lib/?.so;" .. package.cpath
 end
+
+SendKeyCode = require("sendKeyCode")
 
 -- 创建一个固定大小的栈
 local function create_stack(max_size)
@@ -46,32 +40,31 @@ local function on_commit(text, env)
     env.stack:push(length) -- 将长度压入栈
 end
 
--- 主过滤逻辑
----@diagnostic disable-next-line: unused-local
-function T.func(input, seg, env)
-    if input == env.REVERT_KEY then
-        env.engine.context:clear()
-        local n = env.stack:pop() or 1 -- 从栈中弹出一个数字
-        SendKeyCode.press_key("\b", n)
-    elseif input == env.GOTOEND_KEY then
-        env.engine.context:clear()
-        SendKeyCode.press_key("END", 1)
-    end
-end
-
 -- 初始化环境
 function T.init(env)
-    env.REVERT_KEY = "" -- 定义撤回上屏编码
-    env.GOTOEND_KEY = "" -- 定义光标移到末尾编码
-    env.STACK_SIZE = 10 -- 定义栈大小，表示可以连续撤回的最大次数
+    env.REVERT_KEY = ""     -- 定义撤回上屏编码
+    env.GOTOEND_KEY = ""    -- 定义光标移到末尾编码
+    env.STACK_SIZE = 20     -- 定义栈大小，表示可以连续撤回的最大次数
 
-    env.stack = create_stack(env.STACK_SIZE) -- 创建长度为10的栈
+    env.stack = create_stack(env.STACK_SIZE) -- 创建长度为20的栈
     env.revert_notifier = env.engine.context.commit_notifier:connect(function(ctx)
         local text = ctx:get_commit_text()
         if text and text ~= "" then
             on_commit(text, env)
         end
     end)
+end
+
+-- 主过滤逻辑
+function T.func(input, seg, env)
+    if (env.REVERT_KEY:len() > 1) and (input == env.REVERT_KEY) then
+        env.engine.context:clear()
+        local n = env.stack:pop() or 1 -- 从栈中弹出一个数字
+        SendKeyCode.press_key("\b", n)
+    elseif (env.GOTOEND_KEY:len() > 1) and (input == env.GOTOEND_KEY) then
+        env.engine.context:clear()
+        SendKeyCode.press_key("END", 1)
+    end
 end
 
 -- 绑定初始化和资源释放
